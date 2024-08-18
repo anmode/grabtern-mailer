eb07a66fa576f245b01253aad9909d69e66cbbec
Anmol Agrawal <anmol2002.aa1@gmail.com>
2024-08-18 18:32:54 +0530

    setting up the middle ware

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const express = require('express');
+require('dotenv').config();
 const emailRoutes = require('./routes/emailRoutes');
-
+const cors = require('cors');
 const app = express();
+
+// Middleware to restrict API requests in production
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV === 'prod') {
+    const allowedDomain = '.grabtern.in';
+    const host = req.get('Host') || '';
+    if (!host.endsWith(allowedDomain)) {
+      return res.status(403).send('Forbidden: Access is restricted to .grabtern.in');
+    }
+  }
+  next();
+});
+
+// CORS options
+const corsOptions = {
+  origin: process.env.NODE_ENV === 'prod' ? /\.grabtern\.in$/ : '*',
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/send-mail', emailRoutes);
@@ -10,6 +30,7 @@ app.get('/', (req, res) => {
   res.send("Welcome to Grabtern Email Service!");
 });
 
+// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
